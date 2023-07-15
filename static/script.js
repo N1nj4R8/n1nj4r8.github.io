1200e38b3eefcc9e8fce502c4ed63208232ef62c
N1nj4R8 <s1lntlwd@proton.me>
2023-07-15 16:38:29 +0330

    Update content

--- static/script.js
+++ static/script.js
@@ -1,3 +1,13 @@
 function goHome() {
     document.location.href = "/";
 }
+
+function menu() {
+    const links = document.querySelector(".links");
+
+    if (links.style.display === "none") {
+        links.style.display = "block";
+    } else {
+        links.style.display = "none";
+    }
+}
